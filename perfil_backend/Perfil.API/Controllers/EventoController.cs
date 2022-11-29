a7db797031a509d959d89a25c73fd28a86de2d4c
Igor de Oliveira Grandin <igor_grandin@hotmail.com>
2022-11-29 16:04:17 -0300

    Criação e configuração do Back e do Front.

---
+++ perfil_backend/Perfil.API/Controllers/EventoController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Perfil.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Perfil.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+
+        private readonly ILogger<EventoController> _logger;
+
+        public EventoController(ILogger<EventoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IEnumerable<Evento> _evento = new Evento[]
+        {
+            new Evento(){
+                EventoId = 1,
+                Tema = "Angular e ASP.NET",
+                Local = "São Paulo",
+                Lote = "1° Lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                ImagemURL = "foto.png"
+                },
+            new Evento(){
+                EventoId = 2,
+                Tema = "Teclado",
+                Local = "São Paulo",
+                Lote = "2° Lote",
+                QtdPessoas = 500,
+                DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
+                ImagemURL = "fotoTeclado.png"
+                }
+        };
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _evento.Where(evento => evento.EventoId == id );
+        }
+
+        [HttpPost("UpdateEvento")]
+        public IActionResult Post([FromBody] string str)
+        {
+            return Ok();
+        }
+    }
+}
