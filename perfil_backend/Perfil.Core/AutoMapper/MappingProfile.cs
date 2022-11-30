87bc30b993726052f1a6bd89a66df8feedc7b724
Igor de Oliveira Grandin <igor_grandin@hotmail.com>
2022-11-30 11:18:26 -0300

    Criação AutoMapper e Finalização de Serviços.

---
+++ perfil_backend/Perfil.Core/AutoMapper/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Perfil.Core.Models;
+using Perfil.Infrastructure.Entities;
+
+namespace Perfil.Core.AutoMapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<EventosModel, Eventos>();
+            CreateMap<Eventos, EventosModel>();
+        }
+    }
+}
---
+++ perfil_backend/Perfil.Core/Services/EventosService.cs
@@ -0,0 +1,50 @@
+﻿using Perfil.Core.Interfaces;
+using Perfil.Core.Models;
+using Perfil.Infrastructure.Entities;
+using Perfil.Infrastructure.Interface;
+using System;
+using AutoMapper;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using Perfil.Infrastructure.Models;
+
+namespace Perfil.Core.Services
+{
+    public class EventosService : IEventosService
+    {
+        private readonly IEventosRepository _eventosRepository;
+        private readonly IMapper _mapeador;
+        private readonly IOptions<Settings> _settings;
+
+        public EventosService(
+            IEventosRepository eventosRepository,
+            IMapper mapeador,
+            IOptions<Settings> settings)
+        {
+            _eventosRepository = eventosRepository;
+            _mapeador = mapeador;
+            _settings = settings;
+        }
+
+        #region ' Listar Todos os Eventos '
+        public List<EventosModel> ListarEventos()
+        {
+            List<Eventos> listaEventos = _eventosRepository.ListarEventos();
+
+            List<EventosModel> listaModel = _mapeador.Map<List<Eventos>, List<EventosModel>>(listaEventos);
+
+            return listaModel;
+        }
+        #endregion
+
+        #region ' Listar Todos os Eventos '
+        public Eventos GetById(int id)
+        {
+            return _eventosRepository.GetById(id);
+        }
+        #endregion
+    }
+}
